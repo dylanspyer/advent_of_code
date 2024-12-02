aa0a4e48554318beca91282de07b9892a54e3ab4
Dylan Spyer <dylanspyer@gmail.com>
2024-12-02 17:33:12 -0600

    completed day 2 exercise 2 (very inefficiently)

--- day_2/event_2.rb
+++ day_2/event_2.rb
@@ -3,6 +3,7 @@ require 'yaml'
 
 sample_data1 = parse_input(YAML.load_file('input_data.yml')['sample_data1'])
 sample_data2 = parse_input(YAML.load_file('input_data.yml')['sample_data2'])
+sample_data3 = parse_input(YAML.load_file('input_data.yml')['sample_data3'])
 
 def get_direction(num1, num2)
   num2 - num1 >= 0 ? 'increasing' : 'decreasing'
@@ -21,17 +22,13 @@ end
 
 def report_safe?(report)
   report_direction = get_direction(report[0], report[1])
-  bad_level_tolerant = true
 
   report.each_with_index do |num, idx|
     next unless idx.positive?
 
     prev_num = report[idx - 1]
 
-    bad_adjacency = !adjacent_level_tolerance?(prev_num, num)
-    bad_uniformity = !uniform_direction?(prev_num, num, report_direction)
-
-    next unless bad_level
+    next if adjacent_level_tolerance?(prev_num, num) && uniform_direction?(prev_num, num, report_direction)
 
     return false
   end
@@ -39,17 +36,26 @@ def report_safe?(report)
   true
 end
 
-# options for dealing with bad level
-# 1. break of out the loop and start a new loop with the sliced version omitting the bad level
-# 2. recalulate the direction of the report and continue in the same loop
-
 def safe_count(data)
   count = 0
 
-  data.each { |report| count += 1 if report_safe?(report) }
+  data.each do |report|
+    if report_safe?(report)
+      count += 1 if report_safe?(report)
+    else
+      report.length.times do |idx|
+        new_report = report[0...idx] + report[idx + 1...report.length]
+        if report_safe?(new_report)
+          count += 1
+          break
+        end
+      end
+    end
+  end
 
   count
 end
 
-p safe_count sample_data1
+# p safe_count sample_data1
 p safe_count sample_data2
+# p safe_count sample_data3
