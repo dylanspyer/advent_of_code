4b107020933475034bd856ae3bae7f07efc79569
Dylan Spyer <dylanspyer@gmail.com>
2024-12-03 10:37:01 -0600

    solved day 3 exercise 2

--- day_3/event_1.rb
+++ day_3/event_1.rb
@@ -5,11 +5,11 @@ sample_data2 = YAML.load_file('data.yml')['sample_data2']
 
 # xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 
-def parse_input(string)
+def part1(string)
   pattern = /mul\(\d+,\d+\)/
   muls = string.scan(pattern)
-  muls.map { |mul| mul.scan(/\d+/).map(&:to_i).reduce(&:*) }.sum
+  muls.map { |mul| mul.scan(/\d+/).map(&:to_i).reduce(:*) }.sum
 end
 
-p parse_input(sample_data1)
-p parse_input(sample_data2)
+p part1(sample_data1)
+p part1(sample_data2)
