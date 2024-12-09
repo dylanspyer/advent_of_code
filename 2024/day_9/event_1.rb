b472fc18516653f426485381076f0c32c2d84766
Dylan Spyer <dylanspyer@gmail.com>
2024-12-09 13:04:13 -0600

    refactored. messed around with a pattern using ruby's  method for side effects

--- 2024/day_9/event_1.rb
+++ 2024/day_9/event_1.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
-sample_data1 = YAML.load_file('data.yml')['sample_data1']
-sample_data2 = YAML.load_file('data.yml')['sample_data2']
+sample_data1 = YAML.load_file('data.yml')['sample_data1'].strip
+sample_data2 = YAML.load_file('data.yml')['sample_data2'].strip
 # sample_data2 = File.read('input.txt')
 
 # 00...111...2...333.44.5555.6666.777.888899
@@ -29,36 +29,51 @@ sample_data2 = YAML.load_file('data.yml')['sample_data2']
 # - If the element is a number, move that number to the left pointer and set right pointer equal to `.`
 # The process is complete when the two pointers cross each other
 
-def part1(line)
-  line = line.strip
+# 233313312141413140223
+# 00...111...2...333.44.5555.6666.777.888899..10^10^10
+# Maybe use a delimiter instead of an array?
 
-  block = []
+def expand_format(disk_map)
   id = '0'
-  line.each_char.with_index do |c, i|
-    if i.even?
-      block.concat([id] * c.to_i)
-      id = id.to_i + 1
-      id = id.to_s
-    else
-      block.concat(['.'] * c.to_i)
-    end
+  disk_map.each_char.with_index.flat_map do |c, i|
+    i.even? ? ([id] * c.to_i).tap { |_| (id = id.to_i + 1).to_s } : ['.'] * c.to_i
+    # if i.even?
+    #   result = [id] * c.to_i
+    #   id = (id.to_i + 1).to_s
+    #   result
+    # else
+    #   ['.'] * c.to_i
+    # end
   end
-
-  l = 0
-  r = block.length - 1
-
-  while l < r
-    r -= 1 while block[r] == '.'
-    l += 1 while block[l] != '.'
-
-    block[l], block[r] = block[r], block[l]
-  end
-
-  block.each_with_index.reduce(0) { |checksum, (c, i)| checksum + c.to_i * i }
 end
 
-p part1 sample_data2
+def remove_left_gaps(expanded_dm)
+  l = 0
+  r = expanded_dm.length - 1
+
+  while l < r
+    r -= 1 while expanded_dm[r] == '.'
+    l += 1 while expanded_dm[l] != '.'
+
+    expanded_dm[l], expanded_dm[r] = expanded_dm[r], expanded_dm[l]
+  end
+
+  expanded_dm
+end
+
+def calculate_checksum(formatted_dm)
+  formatted_dm.each_with_index.reduce(0) { |checksum, (c, i)| checksum + c.to_i * i }
+end
+
+def part1(disk_map)
+  expanded_format = expand_format(disk_map)
+  formatted_dm = remove_left_gaps(expanded_format)
+
+  calculate_checksum(formatted_dm)
+end
+
 p part1 sample_data1
+p part1 sample_data2
 
 # 0099811188827773336446555566..............
 # 0099811188827773336446555566..............
